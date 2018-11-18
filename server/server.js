b2454474181bab786e7cf0c180303429fdef7648
yichiang <yi_chiang@Outlook.com>
2018-11-17 16:09:51 -0800

    allow cros

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const io = require('socket.io');
 const app = express();
 const server = createServer(app);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', express.static(`${process.cwd()}/../public`));
 console.log(`${process.cwd()}/../build/index.html`)
--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import openSocket from 'socket.io-client';
-const  socket = openSocket('http://localhost:4200');
+var url = 'https://8080-dot-3799353-dot-devshell.appspot.com/?authuser=0' || 'http://localhost:4200'
+const  socket = openSocket(url);
 
 export default socket;
